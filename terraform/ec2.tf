36c680d8c7575c3c429a3274e6eb2ba249e0a263
alpheratz <rifqi@Rifqi.localdomain>
2025-12-05 22:11:17 +0700

    initiate after deploy from terraform

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,5 +1,20 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = [ "099720109477" ] #canonical ubuntu owner
+
+  filter {
+    name = "name"
+    values = [ "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*" ]
+  }
+  filter {
+    name = "architecture"
+    values = [ "x86_64" ]
+  }
+}
+
 resource "aws_instance" "url_shortener" {
-    ami = "ami-0e2ff28bfb72a4e45" #Ubuntu 22.04 ap-southeast-1
+    ami = data.aws_ami.ubuntu.id #Ubuntu 22.04 ap-southeast-1
     instance_type = var.instance_type
     key_name = aws_key_pair.generated.key_name
     security_groups = [aws_security_group.url_sg.name]
--- terraform/keypair.tf
+++ terraform/keypair.tf
@@ -1,11 +1,11 @@
 resource "tls_private_key" "ssh_key" {
     algorithm = "RSA"
-    rsa_bits = 4096
+    rsa_bits = 2048
 }
 
 resource "aws_key_pair" "generated" {
     key_name = var.key_name
-    public_key = tls_private_key.ssh_key.private_key_openssh
+    public_key = tls_private_key.ssh_key.public_key_openssh
 }
 
 resource "local_file" "private_key" {
