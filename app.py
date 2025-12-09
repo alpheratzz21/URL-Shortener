2e702846ff1c283fbcbf8fabb12700159da95c2f
alpheratz <rifqi@Rifqi.localdomain>
2025-12-09 23:04:49 +0700

    update app and terraform config also flask.conf

--- app.py
+++ app.py
@@ -13,13 +13,13 @@ def generate_code(length=6):
 #function -> load and save data
 
 def load_data():
-    if not os.path.exists("storage.json"):
+    if not os.path.exists("/home/ubuntu/URL-Shortener/storage.json"):
         return{}
-    with open("storage.json", "r") as f:
+    with open("/home/ubuntu/URL-Shortener/storage.json", "r") as f:
         return json.load(f)
 
 def save_data(data):
-    with open("storage.json", "w") as f:
+    with open("/home/ubuntu/URL-Shortener/storage.json", "w") as f:
         json.dump(data, f, indent=4)
 
 @app.route("/", methods=["GET", "POST"])
--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -22,22 +22,54 @@ resource "aws_instance" "url_shortener" {
     user_data = <<-EOF
 #!/bin/bash
 apt update -y
-apt install -y nginx python3 python3-pip git
+apt install -y nginx python3 python3-pip git python3-venv
 
 # Clone Repo
 cd /home/ubuntu
 git clone https://github.com/alpheratzz21/URL-Shortener.git
+chown -R ubuntu:ubuntu URL-Shortener
+
 cd URL-Shortener
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
 
-pip3 install flask gunicorn
+#fix storage.json permissions
+if [ ! -f /home/ubuntu/URL-Shortener/storage.json ]; then
+    echo "{}" > /home/ubuntu/URL-Shortener/storage.json
+fi
 
-#Copy nginx config
-cp flask.conf /etc/nginx/sites-available/flask.conf
+chown ubuntu:www-data /home/ubuntu/URL-Shortener/storage.json
+chmod 666 /home/ubuntu/URL-Shortener/storage.json
+
+#update permission gunicorn
+chmod +x /home/ubuntu/URL-Shortener/venv/bin/gunicorn
+
+
+#Create nginx config
+
+# cat <<EOT> /etc/nginx/sites-available/flask.conf
+# server{
+#     listen 80;
+#     server_name _;
+    
+#     location / {
+#         proxy_pass http://127.0.0.1:5000;
+#         proxy_set_header Host \$host;
+#         proxy_set_header X-Real-IP \$remote_addr;
+#     }
+# }
+# EOT
+
+#copy nginx config 
+
+cp /home/ubuntu/URL-Shortener/nginx/flask.conf /etc/nginx/sites-available/flask.conf
 ln -s /etc/nginx/sites-available/flask.conf /etc/nginx/sites-enabled/flask.conf
-rm /etc/nginx/sites-enabled/default
-systemctl restart nginx
+rm -f /etc/nginx/sites-enabled/default
+systemctl restart nginx.service
 
 #gunicorn systemd
+
 cat <<EOT > /etc/systemd/system/gunicorn.service
 [Unit]
 Description=Gunicorn URL Shortener
@@ -45,16 +77,17 @@ After=network.target
 
 [Service]
 User=ubuntu
+Group=www-data
 WorkingDirectory=/home/ubuntu/URL-Shortener
-ExecStart=/usr/bin/python -m gunicorn -b 0.0.0.0:5000 app:app
+ExecStart=/home/ubuntu/URL-Shortener/venv/bin/gunicorn --workers 3 -b 127.0.0.1:5000 app:app
 
 [Install]
 WantedBy=multi-user.target
 EOT
 
 systemctl daemon-reload
-systemctl enable gunicorn
-systemctl start gunicorn
+systemctl enable gunicorn.service
+systemctl start gunicorn.service
 EOF
 
     tags = {
